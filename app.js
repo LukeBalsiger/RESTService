0c09913e7a3dd27deca9e252607c4514e910a39a
Luke Balsiger <lbalsiger@scriptpro.com>
2017-07-27 15:36:58 -0500

    Fixed File Location

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 
-
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     req.send('welcome to my API')
