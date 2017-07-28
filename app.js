eef80418410085a6afb9e1158e28687248fa5965
Luke Balsiger <lbalsiger@scriptpro.com>
2017-07-28 08:28:29 -0500

    First Stable build + gulp

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    req.send('welcome to my API')
+    res.send('welcome to my API');
 });
 
 app.listen(port, function(){
-    console.log('Running on port: ' + port)
+    console.log('Running on port: ' + port);
 });
