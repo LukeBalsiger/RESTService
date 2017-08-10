50277f7ae8c9e597c8496ac7dc28a9a2789fd6c5
Luke Balsiger <lbalsiger@scriptpro.com>
2017-08-10 07:39:01 -0500

    Added tests

--- app.js
+++ app.js
@@ -24,4 +24,3 @@ app.get('/', function(req, res){
 app.listen(port, function(){
     console.log('Running on port: ' + port);
 });
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    gutil = require('gulp-util'),
     nodemon = require('gulp-nodemon');
 
 gulp.task('default', function(){
@@ -11,6 +13,15 @@ gulp.task('default', function(){
         ignore: ['./node_modules/**']
     })
     .on('restart', function(){
-        console.log('Restarting');
-    });
-})
+        console.log('Restarting and running tests...');
+        return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', gutil.log);
+    })
+    .on('start', function(){
+        console.log('Running tests...');
+        return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', gutil.log);
+    })
+});
