cd06f65f9187c967d732f053051bbfd3b2db9e2f
Luke Balsiger <lbalsiger@scriptpro.com>
2017-08-17 13:05:40 -0500

    Added another unit test

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -21,8 +21,23 @@ describe('Card CRUD Test', function(){
             .send(cardPost)
             .expect(200)
             .end(function(err,results){
+                results.body.name.should.equal('Charizard');
                 results.body.nationalPokedexNumber.should.equal(6);
                 results.body.should.have.property('_id');
+                done();
+            })
+    })
+
+    it('should allow a property of a card to be modified and return the same _id and new data', function(done){
+        var cardPost = {name:'Charizard', nationalPokedexNumber:6};
+        var cardPatch = {name: 'Charmander'};
+
+        agent.post('/api/pokemon/cards')
+            .send(cardPost)
+            .send(cardPatch)
+            .end(function(err,results){
+                results.body.name.should.equal('Charmander');
+                results.body.nationalPokedexNumber.should.equal(6);
                 done();
             })
     })
