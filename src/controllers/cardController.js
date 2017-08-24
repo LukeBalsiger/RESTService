76dc2d9ac2bcb15f4c895076dd156b564714a621
Luke Balsiger <lbalsiger@scriptpro.com>
2017-08-24 16:04:51 -0500

    Added more filtering options

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -18,10 +18,25 @@ var cardController = function(Card){
         
         var query = {};
 
-        if(req.query.name)
-        {
-            query.name = req.query.name;
-        }
+        if(req.query.name) query.name = req.query.name;
+        if(req.query.id) query.id = req.query.id;
+        if(req.query.nationalPokedexNumber) query.nationalPokedexNumber = req.query.nationalPokedexNumber;
+        if(req.query.imageUrl) query.imageUrl = req.query.imageUrl;
+        if(req.query.imageUrlHiRes) query.imageUrlHiRes = req.query.imageUrlHiRes;
+        if(req.query.types) query.types = req.query.types;
+        if(req.query.supertype) query.supertype = req.query.supertype;
+        if(req.query.subtype) query.subtype = req.query.subtype;
+        if(req.query.hp) query.hp = req.query.hp;
+        if(req.query.retreatCost) query.retreatCost = req.query.retreatCost;
+        if(req.query.number) query.number = req.query.number;
+        if(req.query.artist) query.artist = req.query.artist;
+        if(req.query.rarity) query.rarity = req.query.rarity;
+        if(req.query.setName) query.setName = req.query.setName;
+        if(req.query.setCode) query.setCode = req.query.setCode;
+        if(req.query.attacks) query.attacks = req.query.attacks;
+        if(req.query.weaknesses) query.weaknesses = req.query.weaknesses;
+        if(req.query.owned) query.owned = req.query.owned;
+
         
         Card.find(query, function(err,cards){
             if(err){
