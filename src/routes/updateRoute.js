48ab16a5d010930a7baacfd31d98d0bbcaf96f9a
Luke Balsiger <lbalsiger@scriptpro.com>
2017-08-30 07:51:45 -0500

    Added update framework

--- src/routes/updateRoute.js
+++ src/routes/updateRoute.js
@@ -1,12 +1,28 @@
-var express = require('express');
+var express = require('express'),
+    http = require('http');
 
-var routes = function(){
+var routes = function(Temp){
     var updateRouter = express.Router();
 
     updateRouter.route('/')
         .post(function(req, res){
-            
+            var tempObj;
+            callback = function(response) {
+                var str = '';
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+                response.on('end', function () {
+                    console.log(str);
+                    res.send(str);
+                    tempObj = new Temp(JSON.parse(str));
+                    console.log(tempObj);
+                });
+            };
+
+            http.request(`http://freegeoip.net/json/${req.body.path}`, callback).end();
         })
+
         .get(function(req, res){
             res.send('Welcome to the update route!');
         });
