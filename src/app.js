10cafe8b53b97d4faa1255416c76eb71b4999d72
LukeBalsiger <lukebalsiger@gmail.com>
2017-09-06 08:59:49 -0500

    asdgfasdgf

--- src/app.js
+++ src/app.js
@@ -3,12 +3,7 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     Card = require('./models/cardModel.js'),
-<<<<<<< HEAD
-    pokemon = require('pokemontcgsdk'),
-    port = process.env.PORT || 3000;
-=======
     port = process.env.PORT || 4000;
->>>>>>> 1c22d8d07e13105d7a68e8cdfbc06da04916b093
 
 var db;
 if(process.env.ENV == 'Test') 
@@ -22,19 +17,11 @@ app.use(bodyParser.json());
 
 cardRouter = require('./routes/cardRoute')(Card);
 pokemonRouter = require('./routes/pokemonRoute')();
-<<<<<<< HEAD
-updateRouter = require('./routes/updateRoute')(pokemon);
-
-app.use('/api/pokemon/cards',cardRouter);
-app.use('/api/pokemon',pokemonRouter);
-app.use('/api/update',updateRouter);
-=======
 updateRouter = require('./routes/updateRoute')();
 
 app.use('/api/pokemon/cards',cardRouter);
 app.use('/api/pokemon',pokemonRouter);
 app.use('/update',updateRouter);
->>>>>>> 1c22d8d07e13105d7a68e8cdfbc06da04916b093
 
 app.get('/api', function(req, res){
     res.send('welcome to my API');
