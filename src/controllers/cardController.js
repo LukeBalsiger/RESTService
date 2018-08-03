fd2cf3ac5b09b7e5da15ab76eaa22149c0771dee
Luke Balsiger <Luke Balsiger>
2018-08-03 15:36:18 -0500

    Made some more changes

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -4,14 +4,18 @@ var display = require('./../helpers/display')
 var cardController = function(pokemon){
 
     var get = function(req, res){
-        pokemon.card.where({ name: req.query.name})
+        var query = {}
+        if(req.query.name) {query.name = req.query.name}
+        if(req.query.set) {query.set = req.query.set}
+        pokemon.card.where(query)
         .then(result => {
             var stuff = {
                 length: result.length,
                 set: [],
                 number: [],
                 rarity: [],
-                imageUrls: []
+                imageUrls: [],
+                name: []
             }
             for(var i = 0; i < result.length; i++)
             {
@@ -19,13 +23,15 @@ var cardController = function(pokemon){
                 stuff.number.push(result[i].number)
                 stuff.rarity.push(result[i].rarity)
                 stuff.imageUrls.push(result[i].imageUrl)
+                stuff.name.push(result[i].name)
             }
             var listSet = display.createLIList(stuff.set)
             var listNum = display.createLIList(stuff.number)
             var listRarity = display.createLIList(stuff.rarity)
             var listUrls = display.createLIList(stuff.imageUrls)
+            var listName = display.createLIList(stuff.name)
 
-            var response = display.createListOfLists([listSet, listNum, listRarity, listUrls])
+            var response = display.createListOfLists([listSet, listNum, listRarity, listUrls, listName])
             res.send(response)
             console.log(result.length)
             console.log('done');
